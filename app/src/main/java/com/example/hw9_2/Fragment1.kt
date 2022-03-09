c6d3436823b0f732ae9d9b61c0683c19834cd368
razi <rezaie127@yahoo.com>
2022-03-09 08:07:09 +0330

    edit views of fragments

--- app/src/main/java/com/example/hw9_2/Fragment1.kt
+++ app/src/main/java/com/example/hw9_2/Fragment1.kt
@@ -1,15 +1,20 @@
 package com.example.hw9_2
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+import com.example.hw9_2.databinding.Fragment1Binding
 
 
 class Fragment1 : Fragment() {
-
-
+    lateinit var binding: Fragment1Binding
+    var gender = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,9 +24,67 @@ class Fragment1 : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding = Fragment1Binding.inflate(inflater, container, false)
+
+        return binding.root
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false)
+        //return inflater.inflate(R.layout.fragment_1, container, false)
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        initView()
+
+
+    }
+
+
+    private fun initView() {
+        binding.register.setOnClickListener {
+
+            if (binding.editTextFullName.text.isBlank())
+                binding.editTextFullName.error = "نام را وارد کنید"
+            else if (binding.editTextUserName.text.isBlank())
+                binding.editTextUserName.error = "نام کاربری را وارد کنید"
+            else if (binding.editTextEmailAddress.text.isBlank())
+                binding.editTextEmailAddress.error = "ایمیل را وارد کنید"
+            else if (binding.editTextPassword.text.isBlank())
+                binding.editTextPassword.error = "پسورد را وارد کنید"
+            else if (binding.editTextPassword.text.toString() != binding.editTextReTypePassword.text.toString()) {
+                binding.editTextReTypePassword.error = "پسوردهای وارد شده یکی نیستند"
+            } else if (!binding.Female.isChecked && !binding.Male.isChecked) {
+                binding.Male.error = "یک گزینه را انتخاب کنید"
+            } else {
+                if (binding.Female.isChecked) {
+                    gender = "Female"
+                } else if (binding.Male.isChecked) {
+                    gender = "Male"
+                }
+
+                goToFragment2()
+            }
+        }
+    }
+
+    private fun goToFragment2() {
+        val fullName = binding.editTextFullName.text.toString()
+        val userName = binding.editTextUserName.text.toString()
+        val email = binding.editTextEmailAddress.text.toString()
+        val password = binding.editTextPassword.text.toString()
+
+
+        val bundle = bundleOf(
+            "fullName" to fullName, "userName" to userName,
+            "email" to email, "password" to password, "gender" to gender
+        )
+
+
+        findNavController().navigate(R.id.action_fragment1_to_fragment2, bundle)
+
+    }
 }
--- app/src/main/java/com/example/hw9_2/Fragment2.kt
+++ app/src/main/java/com/example/hw9_2/Fragment2.kt
@@ -1,14 +1,18 @@
 package com.example.hw9_2
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.hw9_2.databinding.Fragment2Binding
 
 class Fragment2 : Fragment() {
 
-
+    lateinit var  binding : Fragment2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,9 +22,48 @@ class Fragment2 : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding = Fragment2Binding.inflate (inflater, container, false)
+
+        return binding.root
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false)
+        //return inflater.inflate(R.layout.fragment_2, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+    }
+
+    private fun initView() {
+        val fullName=requireArguments().getString("fullName")
+        val userName=requireArguments().getString("userName")
+        val email=requireArguments().getString("email")
+        val password=requireArguments().getString("password")
+        val gender=requireArguments().getString("gender")
+
+        binding.textViewFullName.text=fullName
+        binding.textViewUserName.text=userName
+        binding.textViewEmail.text=email
+        binding.textViewPassword.text=password
+        binding.textViewGender.text=gender
+
+        binding.buttonSave.setOnClickListener {
+            var pref = activity?.getSharedPreferences("share", Context.MODE_PRIVATE)
+            val edit = pref?.edit()
+            edit?.putString("FullName", fullName)
+            edit?.putString("UserName", userName)
+            edit?.putString("Email", email)
+            edit?.putString("Password", password)
+            edit?.putString("Gender", gender)
+            edit?.apply()
+            Toast.makeText(activity, "ذخیره اطلاعات انجام شد", Toast.LENGTH_SHORT).show()
+
+            findNavController().navigate(R.id.action_fragment2_to_fragment1)
+        }
+
+    }
 
 }
